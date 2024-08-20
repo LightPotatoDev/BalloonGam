9631fbbb55a167210cdd5e2c34aae5cd9379d70f
LightPotato99 <kyungyoun7792@gmail.com>
2024-08-20 23:43:22 +0900

    GG

--- scenes/TextureButton.gd
+++ scenes/TextureButton.gd
@@ -1,7 +1,10 @@
 extends TextureButton
+@export var scene_to:String = 'Main'
 
 func _on_pressed():
-	Transition.change_scene('Main')
+	if Global.game_state == Global.STATES.DEFAULT:
+		Transition.change_scene(scene_to)
+		$AudioStreamPlayer.play()
 
 func _on_mouse_entered():
 	pass
