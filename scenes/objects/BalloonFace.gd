ce2e1c176acdb5d99933ab6aeb4e8bc6628e798f
LightPotato99 <kyungyoun7792@gmail.com>
2024-08-20 12:41:45 +0900

    face and some bg

--- scenes/objects/BalloonFace.gd
+++ scenes/objects/BalloonFace.gd
@@ -1,7 +1,20 @@
 extends Node2D
 class_name BalloonFace
 
+@onready var anim = $AnimationPlayer
+
 func on_pos_move(dir:Vector2):
+	anim.stop(false)
+	anim.play('move')
 	$Sprite2D.position -= dir * 32
-	var tween = create_tween()
+	
+	var scale_amount = Vector2(1.2,0.8)
+	if dir.x == 0:
+		scale_amount = Vector2(0.8,1.2)
+	
+	var tween = create_tween().set_parallel(true)
 	tween.tween_property($Sprite2D,"position",Vector2.ZERO,0.05).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_property($Sprite2D,"offset",dir*3,0.05).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_property($Sprite2D,"scale",scale_amount,0.05).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.chain().tween_property($Sprite2D,"scale",Vector2.ONE,0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.chain().tween_property($Sprite2D,"offset",Vector2.ZERO,0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
